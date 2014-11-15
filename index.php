735fcc268165831c334ff75ae836a5058601c79b
cntnn11 <cntnn11@live.cn>
2014-11-15 22:53:34 +0800

    升级4.0

--- index.php
+++ index.php
@@ -12,7 +12,6 @@
  * @var bool
  */
 define('WP_USE_THEMES', true);
-// 我又修改了
-/** Loads the WordPress Environment and Template */
-require('./wp-blog-header.php');
 
+/** Loads the WordPress Environment and Template */
+require( dirname( __FILE__ ) . '/wp-blog-header.php' );
